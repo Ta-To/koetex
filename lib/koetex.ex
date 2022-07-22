4cc38763b9c5fa58fef1384f4fffe53e6ff8bf83
ta.to <61908291+Ta-To@users.noreply.github.com>
2022-07-22 14:45:34 +0900

    ADD new mix application

---
+++ lib/koetex.ex
@@ -0,0 +1,18 @@
+defmodule Koetex do
+  @moduledoc """
+  Documentation for `Koetex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Koetex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/koetex_test.exs
@@ -0,0 +1,8 @@
+defmodule KoetexTest do
+  use ExUnit.Case
+  doctest Koetex
+
+  test "greets the world" do
+    assert Koetex.hello() == :world
+  end
+end
